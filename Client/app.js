03e4916da6961fda6796b0fadc3aad14b09cd451
Jacob Martinez <jmartinez.data@gmail.com>
2020-03-03 08:55:12 -0600

    Added Put to app.js

--- Client/app.js
+++ Client/app.js
@@ -2,7 +2,7 @@
     function processForm( e ){
         var dict = {
             Title : this["title"].value,
-            Genre : this["genre"].value,            
+            Genre : this["genre"].value,
         	Director: this["director"].value
         };
 
@@ -24,4 +24,32 @@
     }
 
     $('#my-form').submit( processForm );
+    //PUT
+    function editMovie( id ){
+        var dict = {
+            Title : this["title"].value,
+            Genre : this["genre"].value,
+        	Director: this["director"].value
+        };
+
+        $.ajax({
+            url: 'https://localhost:44325/api/movie' + id,
+            dataType: 'json',
+            type: 'put',
+            contentType: 'application/json',
+            data: JSON.stringify(dict),
+            success: function( data, textStatus, jQxhr ){
+                $('title').val('');
+                $('genre').val('');
+                $('director').val('');
+
+                // Call GET here
+            },
+            error: function( jqXhr, textStatus, errorThrown ){
+                console.log( errorThrown );
+            }
+        });
+
+        e.preventDefault();
+    }
 })(jQuery);
