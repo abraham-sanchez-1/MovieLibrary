3fb512b6f652d9c4c94a368c524edf6b30cfdc49
Jacob Martinez <jmartinez.data@gmail.com>
2020-03-04 09:09:41 -0600

    Fixed sizing of modal, slight formatting changes to app.js

--- Client/app.js
+++ Client/app.js
@@ -40,9 +40,7 @@ function processForm( e ){
             contentType: 'application/json',
             data: JSON.stringify(dict),
             success: function( data, textStatus, jQxhr ){
-                $('title').val('');
-                $('genre').val('');
-                $('director').val('');
+                $('#editForm').html(data);
 
                 getMovies();
             },
@@ -77,14 +75,15 @@ function createTable(data, textStatus, jQxhr){
               "<td>" + movie.title + "</td>" +
               "<td>" + movie.genre + "</td>" +
               "<td>" + movie.director + "</td>" +
-              "<td>" + createButtonWithId(movie.movieId) + "</td>" + 
+              "<td>" + createButtonWithId(movie.movieId) + "</td>" +
             "</tr>"
         );
     });
 }
 
 function createButtonWithId(id){
-    let button = '<a href="#ex1" rel="modal:open"><button class="btn btn-success btn-sm" value="' + id + '" onclick=changeModalForm(this.value)>Edit</button></a>';
+    let button = '<a href="#ex1" rel="modal:open"><button class="btn btn-success btn-sm" value="' +
+                  id + '" onclick=changeModalForm(this.value)>Edit</button></a>';
     console.log(button);
     return button;
 }
@@ -93,8 +92,12 @@ function createModal(){
     return modal;
 }
 function changeModalForm(id){
-    $.get(("https://localhost:44325/api/movie/"+id), function(data){
-        document.getElementById("editForm").innerHTML =   '<input class="form-control" type="text" name="title" placeholder="'+data.title +'"/><input class="form-control" type="text" name="genre" placeholder="'+data.genre +'" /><input class="form-control" type="text" name="director" placeholder="'+data.director +'" />';
+    $.get(("https://localhost:44325/api/movie/"+id),
+    function(data){
+        document.getElementById("editForm").innerHTML = '<input class="form-control" type="text" name="title" placeholder="' +
+              data.title + '"/><input class="form-control" type="text" name="genre" placeholder="' +
+              data.genre +'" /><input class="form-control" type="text" name="director" placeholder="' +
+              data.director +'" /><button type="submit" class="btn btn-outline-primary" >Save</button>';
     });
 }
 
